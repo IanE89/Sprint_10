d302002cc54392cd1d296be28747609ef4bf69f7
IanE89 <iansevans89@proton.me>
2023-05-19 17:48:53 -0500

    added sqlite and a simple query.

--- module_1/sqlite_example.py
+++ module_1/sqlite_example.py
@@ -8,7 +8,7 @@ connection = sqlite3.connect('rpg_db.sqlite3')
 cursor = connection.cursor()
 
 # Step 3 - Write a query
-# (See the queries.py file, have moved query there now)
+# (See the queries.py file, moved query there)
 
 # Step 4 - Execute the query on the cursor and fetch the results
 # "pulling results" from the cursor
