4aec2b1de88b07eae84dbabc36830dd4aab176b4
IanE89 <iansevans89@proton.me>
2023-05-19 17:30:53 -0500

    first commit

---
+++ module_1/sqlite_example.py
@@ -0,0 +1,18 @@
+import sqlite3
+import queries as q
+
+# Step 1 - connect to the database
+connection = sqlite3.connect('rpg_db.sqlite3')
+
+# Step 2 - Make the "cursor"
+cursor = connection.cursor()
+
+# Step 3 - Write a query
+# (See the queries.py file)
+
+# Step 4 - Execute the query on the cursor and fetch the results
+# "pulling results" from the cursor
+results = cursor.execute(q.SELECT_ALL).fetchall()
+
+if __name__ == '__main__':
+    print(results[:5])
